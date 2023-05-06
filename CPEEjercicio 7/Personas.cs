5812d89606d878fc616b128288276e4c247806ff
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 21:47:46 -0600

    Agregar archivos de proyecto.

---
+++ CPEEjercicio 7/Personas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPEEjercicio_7
+{
+    internal class Personas
+    {
+        string nom;
+        public Personas() { }
+        public Personas(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+
+        public override string ToString()
+        {
+            return "Nom: " + this.Nom;
+        }
+    }
+    class Profesor:Personas
+    {
+        public Profesor(string nombre)
+        {
+            this.Nom = nombre;
+        }
+
+
+        public void Explicar()
+        {
+            Console.WriteLine($"{Nom} está explicando.");
+        }
+    }
+    class Estudiante : Personas
+    {
+
+        public Estudiante(string nombre)
+        {
+            this.Nom = nombre;
+        }
+
+        public void Estudiar()
+        {
+            Console.WriteLine(this.Nom + " está estudiando.");
+        }
+    }
+}
---
+++ CPEEjercicio 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPEEjercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personas[] p1 = new Personas[3];
+            p1[0] = new Profesor("Juan");
+            p1[1] = new Estudiante("Sara");
+            p1[2] = new Estudiante("Carlos");
+            
+            foreach(Personas p in p1)
+            {
+                if(p is Estudiante)
+                {
+                    ((Estudiante)p).Estudiar();
+                }
+                else
+                {
+                    ((Profesor)p).Explicar();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
